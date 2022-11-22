18e0567be6529f61ddb01d476e3123a5ef7f6b57
MatiC26 <maticarignano3@gmail.com>
2022-11-22 01:05:28 -0300

    .

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+from AppCoder.models import Curso
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+   return render(request, "AppCoder/index.html")
+
+def cursos(request):
+   return HttpResponse("Estas en cursos")
+
+def estudiantes(request):
+   return HttpResponse("Estas en estudiantes")
+
+def profesores(request):
+   return HttpResponse("Estas en profesores")
+    
+    
+def entregables(request):
+   return HttpResponse("Estas en entregables")
+    
+pass
--- django_portafolio/urls.py
+++ django_portafolio/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from listado.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alumnos/', lista_alumnos2)
+    path('alumnos/', lista_alumnos2),
+    
+    path("coder/", include("AppCoder.urls"))
 ]
